fc9a60413f0a73cf711984c79c5c7bd22af85b9c
Samson NIYONIZERA <nisammy40@gmail.com>
2025-09-21 00:01:48 +0200

    adde menu

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,33 +1,75 @@
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { Tabs } from "expo-router";
-import { Image, ImageBackground, Text } from "react-native";
+import { Image, ImageBackground, Text, View } from "react-native";
 
-const TabIcon = () => {
+const TabIcon = ({ focused, icon, title }: { focused: boolean, icon: any, title: string }) => {
+    if (focused) {
+        return (
+            <ImageBackground
+                source={images.highlight}
+                className="flex mx-3 flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
+            >
+                <Image
+                    source={icon}
+                    tintColor="#151312"
+                    className="size-5"
+                />
+                <Text className="text-secondary text-base font-semibol ml-2">
+                    {title}
+                </Text>
+            </ImageBackground >
+        )
+    }
     return (
-        <ImageBackground
-            source={images.highlight}
-            className="flex mx-3 flex-row w-full flex-1 min-w-[112px] min-h-12 mt-4 justify-center items-center rounded-full overflow-hidden"
-        >
+        <View
+            className="size-full justify-center items-center mt-4 rounded-full overflow-hidden">
             <Image
-                source={icons.home}
-                tintColor="#151312"
+                source={icon}
+                tintColor="#fff"
                 className="size-5"
             />
-            <Text className="text-secondary text-base font-semibol ml-2d">Home</Text>
-        </ImageBackground >
+        </View>
     )
 }
 export default function RootLayout() {
     return (
-        <Tabs>
+        <Tabs
+            screenOptions={{
+                tabBarShowLabel: false,
+                tabBarItemStyle: {
+                    width: "100%",
+                    height: "100%",
+                    justifyContent: "center",
+                    alignItems: "center"
+                },
+                tabBarStyle: {
+                    backgroundColor: "#0f0d23",
+                    borderRadius: 50,
+                    marginHorizontal: 20,
+                    marginBottom: 36,
+                    height: 52,
+                    position: "absolute",
+                    overflow: "hidden",
+                    borderWidth: 1,
+                    borderColor: '0f0d23',
+
+
+
+                }
+            }}
+        >
             <Tabs.Screen
                 name="index"
                 options={{
                     title: "Home",
                     headerShown: false,
                     tabBarIcon: ({ focused }) => (
-                        <TabIcon />
+                        <TabIcon
+                            focused={focused}
+                            icon={icons.home}
+                            title="Home"
+                        />
                     )
                 }}
             />
@@ -37,7 +79,11 @@ export default function RootLayout() {
                     title: "Search",
                     headerShown: false,
                     tabBarIcon: ({ focused }) => (
-                        <TabIcon />
+                        <TabIcon
+                            focused={focused}
+                            icon={icons.search}
+                            title="Search"
+                        />
                     )
                 }}
             />
@@ -46,6 +92,14 @@ export default function RootLayout() {
                 options={{
                     title: "Saved",
                     headerShown: false,
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon
+                            focused={focused}
+                            icon={icons.save}
+                            title="Saved"
+                        />
+                    )
+
                 }}
             />
             <Tabs.Screen
@@ -53,6 +107,13 @@ export default function RootLayout() {
                 options={{
                     title: "Profile",
                     headerShown: false,
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon
+                            focused={focused}
+                            icon={icons.person}
+                            title="Profile"
+                        />
+                    )
                 }}
             />
         </Tabs >
